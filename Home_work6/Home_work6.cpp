0a43e89a9e32cb6f9b10ca97f73ebcb713de72f5
Myroslav229 <135414395+Myroslav229@users.noreply.github.com>
2023-07-09 16:41:25 +0300

    Update Home_work6.cpp

--- Home_work6/Home_work6.cpp
+++ Home_work6/Home_work6.cpp
@@ -1,20 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime> 
 
 int main()
 {
+    std::cout << "Game start\n";
     while (true) {
-        std::cout << "Game start\nEnter -1 for exit\nEnter -2 for Treasure hunter\nTry to guess number (0-50)";
+        std::cout << "Ultimate quess quest\nEnter -1 for exit\nEnter -2 for Treasure hunter\nTry to guess number (0-50)\n";
         int highscore = 0;
         int score = 0;
         int ans = 0;
         int ansrow = 0, anscol = 0;
+        char field[7][7];
+        int row = 0, col = 0;
         while (true)
         {
             score = 0;
             std::srand(std::time(nullptr));
             const int GeneratingRange = 50;
-            const double generatednum = std::rand() % (GeneratingRange - 1); 
+            const double generatednum = std::rand() % (GeneratingRange - 1);
             ans = 0;
             while (true) {
                 score++;
@@ -55,7 +58,7 @@ int main()
         if (ans == -2) {
             score = 0;
 
-            std::cout << "Treasure hunter\nTry to guess the coordinates of the treasure (0-7)x(0-7)\n Enter -1 0 for exit\n Enter 0 -1 for original game ";
+            std::cout << "Treasure hunter\nTry to guess the coordinates of the treasure (0-7)x(0-7)\n Enter -1 -1 for exit\n Enter -1 0 for original game ";
             while (true) {
                 std::srand(std::time(nullptr));
                 const int GeneratingRange = 7;
@@ -63,27 +66,30 @@ int main()
                 const double generatedcol = std::rand() % (GeneratingRange - 1);
                 std::cout << generatedrow << ',' << generatedcol << std::endl;
                 while (true) {
-                    char field[7][7];
-                    int row = 0, col = 0;
+                    std::cin >> ansrow >> anscol;
                     if (ansrow == -1 || anscol == -1) break;
-                    for (row = 0; row < 7; ++row) {
-                        for (col = 0; col < 7; col++) {
-                            if (generatedrow == ansrow && generatedcol == anscol && ansrow == row && anscol == col)
-                                field[row][col] = { 'x' };
-                            else if (ansrow == row && anscol == col && ansrow != generatedrow && anscol != generatedcol)
-                                field[row][col] = { '0' };
-                            else if (ansrow == row && ansrow == generatedrow && anscol != generatedcol && anscol == col)
-                                field[row][col] = { 'r' };
-                            else if (anscol == col && anscol == generatedcol && ansrow != generatedrow && ansrow == row)
-                                field[row][col] = { 'c' };
-                            else field[row][col] = { '#' };
+                    else if (anscol < -1 || ansrow < -1) std::cout << "Entered wrong number\n";
+                    else {
+                        for (row = 0; row < 7; ++row) {
+                            for (col = 0; col < 7; col++) {
+                                if (generatedrow == ansrow && generatedcol == anscol && ansrow == row && anscol == col)
+                                    field[row][col] = { 'x' };
+                                else if (ansrow == row && anscol == col && ansrow != generatedrow && anscol != generatedcol)
+                                    field[row][col] = { '0' };
+                                else if (ansrow == row && ansrow == generatedrow && anscol != generatedcol && anscol == col)
+                                    field[row][col] = { 'r' };
+                                else if (anscol == col && anscol == generatedcol && ansrow != generatedrow && ansrow == row)
+                                    field[row][col] = { 'c' };
+                                else field[row][col] = { '#' };
+                            }
                         }
-                    }
-                    for (row = 0; row < 7; ++row) {
-                        for (col = 0; col < 7; col++) {
-                            std::cout << (char)field[row][col];
+
+                        for (row = 0; row < 7; ++row) {
+                            for (col = 0; col < 7; col++) {
+                                std::cout << (char)field[row][col];
+                            }
+                            std::cout << std::endl;
                         }
-                        std::cout << std::endl;
                     }
                     score++;
                     if (ansrow != generatedrow && anscol != generatedcol) std::cout << "You miss, try again: ";
@@ -101,10 +107,10 @@ int main()
                     highscore = score;
                 }
                 std::cout << "Highscore: " << highscore << std::endl;
-                if (ansrow == -1 || anscol == -1) break;
+                if (ansrow == -1) break;
             }
         }
-        if (ansrow == -1 && anscol == 0) break;
+        if (ansrow == -1 && anscol == -1) break;
     }
     std::cout << "See you next time!";
 }
